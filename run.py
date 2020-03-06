b73ac8bba6b2c5de3d3c0e9b84939b9eeae243c2
treeandgrass <mymymymypcy@163.com>
2020-03-06 20:08:09 +0800

    feat: python script

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
 import subprocess
+import argparse
+import os
 
-subprocess.run(["./openglstudy"]);
+def readFileList(directory):
+    if os.path.isdir(directory) is None:
+        raise EnvironmentError("is not directory"%directory)
+    directory = os.path.join(os.getcwd(), directory)
+    newtFileList = list()
+    fileList = os.listdir(directory)
+    for f in fileList:
+       newtFileList.append(os.path.join(directory, f))
+    return newtFileList
+
+
+
+
+def run(path):
+    l = list()
+    l.append(path)
+    subprocess.run(l)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", help="input exec file dirctory")
+    parser.add_argument("--e", help="input file index")
+    parser.add_argument("--mk", action = "store_true", help="run make commend")
+    args = parser.parse_args()
+    print(args)
+    fileList = readFileList(args.dir)
+    execf = args.e
+
+    # make
+    if args.mk:
+        run("make")
+    # run specific command
+    for f in fileList:
+        if f.find(execf):
+            run(f)
+            break
--- src/journey/hello_window/hello_window.cpp
+++ src/journey/hello_window/hello_window.cpp
@@ -1,81 +1,55 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
 
-#include <iostream>
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-int main()
-{
-    // glfw: initialize and configure
-    // ------------------------------
+int main(int argv, char** argc) {
     glfwInit();
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
+    // create window
+    GLFWwindow *win = glfwCreateWindow(800, 600, "window", NULL, NULL);
+    if (win == NULL) {
         glfwTerminate();
+
         return -1;
     }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwMakeContextCurrent(win);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+    // initialize glad
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "failed initialize glad" << std::endl;
+
         return -1;
-    }    
+    }
 
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // input
-        // -----
-        processInput(window);
+    glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
+    while(!glfwWindowShouldClose(win)) {
+        processInput(win);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(win);
         glfwPollEvents();
     }
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
